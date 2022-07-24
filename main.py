db7cc96dcb4956c57b893a2915afaab991c8daa5
Kristýna Novotná <tynadanielova@seznam.cz>
2022-07-24 07:43:19 -0700

    WIP

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ email: tynadanielova@seznam.cz
 discord: KristýnaN#4503
 """
 
-import task_template
+from task_template import TEXTS
 
 separator = "-" * 40
 new_line = "\n"
-registred_users = {
+registered_users = {
     "bob": "123",
     "ann": "pass123",
     "mike": "password123",
@@ -19,10 +19,25 @@ registred_users = {
 
 username = input("username: ").lower()
 password = input("password: ").lower()
-if username in registred_users.keys() and password in registred_users[username]:
+
+def analyze_text(n):
+    """ Function for analyzing default texts according user's input. """
+    index_of_text = int(number_of_text) - 1
+    for word in TEXTS[index_of_text]:
+        pass
+
+if username in registered_users.keys() and password in registered_users[username]:
     print(separator)
     print(f"Welcome to the app, {username} {new_line}We have 3 text to be analyzed.")
     print(separator)
+    number_of_text = input("Enter a number btw. 1 and 3 to select: ")
+    print(separator)
+    if not number_of_text.isdigit() or int(number_of_text) > 3 or int(number_of_text) <= 0:
+        print("given input is not correct, terminating the program...")
+        exit()
+    else:
+        pass
 else:
     print("unregistered user, terminating the program...")
     exit()
+print(analyze_text(1))
